cdceb2a27a754a90872e224f250428cdf5fd4ccc
Bernal30 <abrn7311@gmail.com>
2025-03-17 17:55:16 -0600

    Adding features to display list of names

--- app.js
+++ app.js
@@ -7,11 +7,27 @@ function agregarAmigo() {
 
     //si el input está vacío
     if(nombreUsuario === "") {
-        alert("Ingrese un nombre valido por favor.")
+        return alert("Ingrese un nombre valido por favor.");
     } else {
-        //de lo contrario se añade el nombre al final del array y se reinicia el input
+        //de lo contrario se añade el nombre al final del array
         amigos.push(nombreUsuario);
     }
+    //se reinicia el input
     document.getElementById("amigo").value = "";
     console.log(amigos);
+
+    displayedList();
+
+    return;
+}
+
+//función para visualizar la lista en la pantalla
+function displayedList() {
+    //Seleccion y limpieza de la lista existente
+    let displayedList = document.getElementById("listaAmigos");
+    displayedList.innerHTML = "";
+    //iteración sobre el arreglo para agregar nombres en cada iteración 
+    for (let index = 0; index < amigos.length; index++) {
+        displayedList.innerHTML += `<li class="item-list"> ${amigos[index]} </li>`;
+    }    
 }
