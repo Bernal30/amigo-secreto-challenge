d615b706ae3da3cffd7d4ccdc92801a0eb31319a
Bernal30 <abrn7311@gmail.com>
2025-03-17 15:45:24 -0600

    Adding the first features

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+//función para agregar elementos al array
+function agregarAmigo() {
+    //se toma el valor del input
+    let nombreUsuario = document.getElementById("amigo").value;
+
+    //si el input está vacío
+    if(nombreUsuario === "") {
+        alert("Ingrese un nombre valido por favor.")
+    } else {
+        //de lo contrario se añade el nombre al final del array y se reinicia el input
+        amigos.push(nombreUsuario);
+    }
+    document.getElementById("amigo").value = "";
+    console.log(amigos);
+}
